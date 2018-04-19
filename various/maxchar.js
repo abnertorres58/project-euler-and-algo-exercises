eea7c45f67dbc6cb751c9be7560b6ad8575cd86e
Freud Romero <abnertorres58@gmail.com>
2018-04-18 23:40:56 -0400

    given a string returns the most common

--- various/maxchar.js
+++ various/maxchar.js
@@ -1,8 +1,22 @@
-var arr = [1, 2, 3, 4, 5];
-var x = {}
+var str = 'apple 1231111';
+var mapChar = {};
+var max = 0;
+var c;
 
-for(let num of arr){
-  x[num] = 1;
+
+for(let char of str){
+  if(mapChar[char]){
+    mapChar[char]++;
+  } else {
+    mapChar[char] = 1;
+  };
 }
+ for(let char in mapChar){
 
-console.log(x)
+  if(mapChar[char] > max){
+    max = mapChar[char]
+    c = char;
+  }
+ }
+
+ console.log(c)
